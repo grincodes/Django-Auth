73155216021aca9a63c16a25eb8e98573001da93
designerseyi <designerseyi@gmail.com>
2023-05-04 12:17:58 +0100

    django auth

--- django_auth_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoAuthAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "django_auth_app"
---
+++ django_auth_proj/urls.py
@@ -0,0 +1,20 @@
+"""
+URL configuration for django_auth_proj project.
+
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+ 
+urlpatterns = [
+    
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home/', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('admin/', admin.site.urls),
+]
+
